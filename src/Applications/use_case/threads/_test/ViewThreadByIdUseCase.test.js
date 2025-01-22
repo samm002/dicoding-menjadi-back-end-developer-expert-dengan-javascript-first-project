c59efc9ae76ff2243c831890f725e9c403801f47
samm002 <samuelsdjodis@gmail.com>
2025-01-22 08:59:29 +0700

    fix: updating based on revision 4

--- src/Applications/use_case/threads/_test/ViewThreadByIdUseCase.test.js
+++ src/Applications/use_case/threads/_test/ViewThreadByIdUseCase.test.js
@@ -5,24 +5,6 @@ const ThreadRepository = require('../../../../Domains/threads/ThreadRepository')
 describe('ViewThreadByIdUseCase', () => {
   it('should view thread detail correctly', async () => {
     // Arrange
-    // Raw data that would come from the repository
-    const mockThreadData = [
-      {
-        id: 'thread-123',
-        title: 'Thread 1',
-        body: 'Isi dari Thread 1',
-        updated_at: '2025-01-08 10:30:26.511437',
-        username: 'dicoding',
-        comment_id: null,
-        comment_username: null,
-        comment_date: null,
-        content: null,
-        is_deleted: null,
-        parent_comment_id: null,
-      },
-    ];
-
-    // Expected DetailThread instance that should be returned by use case
     const expectedDetailThread = new DetailThread([
       {
         id: 'thread-123',
@@ -42,7 +24,7 @@ describe('ViewThreadByIdUseCase', () => {
     const mockThreadRepository = new ThreadRepository();
     mockThreadRepository.viewThreadById = jest
       .fn()
-      .mockImplementation(() => Promise.resolve(mockThreadData));
+      .mockImplementation(() => Promise.resolve(expectedDetailThread));
 
     const viewThreadByIdUseCase = new ViewThreadByIdUseCase({
       threadRepository: mockThreadRepository,
@@ -51,9 +33,12 @@ describe('ViewThreadByIdUseCase', () => {
     // Action
     const viewThreadById = await viewThreadByIdUseCase.execute('thread-123');
 
+    console.log(viewThreadById);
+    console.log(expectedDetailThread);
+
     // Assert
     expect(viewThreadById).toBeDefined();
-    expect(new DetailThread(viewThreadById)).toStrictEqual(
+    expect(viewThreadById).toStrictEqual(
       expectedDetailThread
     );
 
